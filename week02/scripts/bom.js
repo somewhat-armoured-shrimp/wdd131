a9b77b9bc3f63cc93b2f1c0fe72ccaf26a36e6ea
somewhat-armoured-shrimp <tor24017@byui.edu>
2024-09-26 16:06:23 -0700

    bom but it works

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,28 +2,28 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list") //what????? ok
 
-const li = document.createElement("li");
-
-const deleteButton = document.createElement("button");
-
 // Create a click event listener for the Add Chapter button using an addEventListener.
 button.addEventListener("click", function () {
     // Code to execute when the button is clicked
     if (input.value.trim() !== "") {
 
+        const deleteButton = document.createElement("button");
+
+        const li = document.createElement("li");
+
         li.textContent = input.value;
 
         deleteButton.textContent = "❌";
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
 
         li.append(deleteButton);
 
         list.append(li);
     }
-
-    deleteButton.addEventListener('click', function () {
-        list.removeChild(li);
-        input.focus();
-    });
 
     input.value = "";
     input.focus();
